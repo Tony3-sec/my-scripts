4c4ecd6aba7f770337b9ec19cd400865cbe49989
Tony36 <tatsuhiko36@gmail.com>
2019-08-18 18:44:19 +0800

    Update IPtoHex.py

--- string-converter/IPtoHex.py
+++ string-converter/IPtoHex.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import sys
-import binascii
 
-## Convert IP address to hexadecimal
+'''
+Convert IP address to hexadecimal
+'''
 
 if len(sys.argv) < 2:
 	print('Please type IP address')
