4ef3f8aaad60ad27e7c5ffb174f5424bfbafbf8d
Tony36 <tatsuhiko36@gmail.com>
2021-01-24 21:08:55 +0800

    Update XOR.py

--- encoder-decoder/XOR.py
+++ encoder-decoder/XOR.py
@@ -2,14 +2,12 @@
 
 import binascii
 
-#data = "79746a6512051b1e187b68036563606603190d7e76730c17746a736667"
 data = "f02a1dfb6521d96509ed211aec361ba92c1bb36559b07746b87350a77458a77448e12a1bfd2b09e42048e03648de0c26a40d27ce0037ef30"
 data = bytearray(binascii.unhexlify(data))
 data_length = len(data)
 
 XORed_bytearray = bytearray(data_length)
 
-#key = "23"
 key = "894568"
 key = bytearray(binascii.unhexlify(key))
 key_length = len(key)
